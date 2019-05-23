4ec8cfc5db0d8c798b345e6d84bd4de6802e08ee
Shuza <shuza.sa@gmail.com>
2019-05-24 00:05:01 +0600

    main file added

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
-func main(){
+/**
+ *  := 	create date: 24-May-2019
+ *  := 	(C) CopyRight Shuza
+ *  := 	shuza.ninja
+ *  := 	shuza.sa@gmail.com
+ *  := 	Code	:	Coffee	: Fun
+ **/
+
+func main() {
 	fmt.Println("Hi")
 }
